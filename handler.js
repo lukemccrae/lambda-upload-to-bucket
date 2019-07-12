800d423531861ad95b53ce6dcf6e1f661a0b25fa
“lukemccrae” <lukemccrae@gmail.com>
2019-07-12 14:26:26 -0700

    unfinished csv uploader

--- handler.js
+++ handler.js
@@ -1,18 +1,32 @@
 'use strict';
+var AWS = require('aws-sdk');
+
+const BUCKET = 'csv-to-json-intuit-challenge';
+const OBJECTKEY = 'file.txt';
 
 module.exports.hello = async event => {
+  var params = {
+   ACL: "authenticated-read",
+   Body: JSON.stringify({csv: event.body},null,2),
+   Bucket: BUCKET,
+   Key: "csv"
+  };
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      return {
+        body: err
+      }
+    } // an error occurred
+    else     console.log(data);           // successful response
+    /*
+    data = {
+     ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+     VersionId: "Kirh.unyZwjQ69YxcQLA8z4F5j3kJJKr"
+    }
+    */
+  });
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    body: err),
+  }
 };
